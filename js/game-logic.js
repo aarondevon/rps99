6e4f7807f77fbea54735af33a2c6e29388ce394e
Aaron Sawyer <me@aarondevon.com>
2019-06-18 09:02:59 -0500

    add method to set player moves and add method that checks for a valid move type

--- js/game-logic.js
+++ js/game-logic.js
@@ -15,3 +15,37 @@ let playerTwoMoveOneValue;
 let playerTwoMoveTwoValue;
 let playerTwoMoveThreeValue;
 
+const validMoves = ["rock", "paper", "scissors"];
+
+// Check for valid move type
+const isValidMove = (moveOneType, moveTwoType, moveThreeType) => {
+	if (!(validMoves.includes(moveOneType)) || !(validMoves.includes(moveTwoType)) || !(validMoves.includes(moveThreeType))) {
+		return false;
+	}
+
+	return true;
+};
+
+const setPlayerMoves = (player, moveOneType, moveOneValue, moveTwoType, moveTwoValue, moveThreeType, moveThreeValue) => {
+	if (!isValidMove(moveOneType, moveTwoType, moveThreeType)) {
+		return;
+	}
+
+	if (player === "Player One") {
+		playerOneMoveOneType = moveOneType;
+		playerOneMoveTwoType = moveTwoType;
+		playerOneMoveThreeType = moveThreeType;
+
+		playerOneMoveOneValue = moveOneValue;
+		playerOneMoveTwoValue = moveTwoValue;
+		playerOneMoveThreeValue = moveThreeValue;
+	} else if (player === "Player Two") {
+		playerTwoMoveOneType = moveOneType;
+		playerTwoMoveTwoType = moveTwoType;
+		playerTwoMoveThreeType = moveThreeType;
+
+		playerTwoMoveOneValue = moveOneValue;
+		playerTwoMoveTwoValue = moveTwoValue;
+		playerTwoMoveThreeValue = moveThreeValue;
+	}
+};
